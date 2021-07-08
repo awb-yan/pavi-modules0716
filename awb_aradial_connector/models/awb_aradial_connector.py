80e428348f0f4289d39dc3c09d94016afdd24381
Aeriane Charmaine Dizon <aeriane.dizon@achievewithoutborders.com>
2021-07-08 16:09:11 +0800

    test query for new accounts

--- awb_aradial_connector/models/awb_aradial_connector.py
+++ awb_aradial_connector/models/awb_aradial_connector.py
@@ -15,9 +15,9 @@ class AWBAradialConnector(models.Model):
     def create_user(self):
 
         sql = """
-            SELECT subs.name, subs.code AS userid
+            SELECT subs.name, subs.code AS userid, line.product_id
             FROM sale_subscription AS subs
-            WHERE subs.date IS NULL
+            WHERE line.product_id IN (subs.recurring_invoice_line_ids)
             LIMIT 1
         """
         # , line.display_name as offer
